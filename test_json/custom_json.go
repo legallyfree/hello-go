546d7f32f71a2b788d38ab55ee2e577a6f3a52f2
legallyfree <legallyfreee@gmail.com>
2021-07-28 23:25:11 -0300

    add customer handler xml - json

---
+++ test_json/custom_json.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func IndexHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(200, gin.H{
+		"message": "hello " + name,
+	})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/:name", IndexHandler)
+	router.Run()
+}
---
+++ test_xml/custom_xml.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", IndexHandler)
+	router.Run()
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
+}
+
+func IndexHandler(c *gin.Context) {
+	c.XML(200, Person{FirstName: "Mohamed", LastName: "Labouardy"})
+}
